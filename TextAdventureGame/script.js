7c569d13bf0e75307f00c3bb134e0ed5d63a88df
AwpData <trevorntang@msn.com>
2020-05-21 19:47:08 -0700

    Added Exercise 8 Assignment

--- TextAdventureGame/script.js
+++ TextAdventureGame/script.js
@@ -1,10 +1,12 @@
+let inputBox = document.getElementById("input-text"); // Text input-box
+
 function getInput(form) {
     let input = form.inputBox.value;
     alert("You typed: " + input);
-    document.getElementById("input-text").value = "";
+    inputBox.value = "";
 }
 
-document.getElementById("input-text").addEventListener("keyup", event => {
+inputBox.addEventListener("keyup", (event) => {
     event.preventDefault();
     if (event.key === "Enter") {
         document.getElementById("submit").click();
