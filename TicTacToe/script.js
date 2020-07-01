7921fe1ebe31947d7ec97d2ea336dbca76b34228
AwpData <trevorntang@msn.com>
2020-06-30 23:10:16 -0700

    Fixed Some Formatting Issues

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,18 +1,44 @@
-window.addEventListener("keyup", (event) => {
-    if (event.key === "o") {
-        document.getElementById("box-2").innerHTML = "5";
-    } else if (event.key === "p") {
-        document.getElementById("box-2").innerHTML = "";
-    }
+window.onload = () => {
+    checkBrowser();
+}
+
+let currentMove = "X";
+
+// NOTE: Do not forget the '.' for querySelectorAll! (they are classes)
+document.querySelectorAll(".box").forEach(item => {
+    // Adds a click event to each individual box
+    item.addEventListener("click", () => {
+        let id = document.getElementById(item.id);
+        if (id.innerHTML === "") {
+            id.innerHTML = currentMove;
+            swapPlayer(id);
+        } else if (id.innerHTML === "X" || id.innerHTML === "O") { // Remove this later, this is for testing purposes
+            id.innerHTML = "";
+        }
+    });
 });
 
-let boxes = document.getElementsByClassName("box");
-
-for (let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("click", addX(boxes[i]), false);
+// Just changes the icon for each player and the color do distinguish them
+function swapPlayer(id) {
+    if (currentMove === "X") {
+        id.style.color = "blue";
+        currentMove = "O";
+    } else if (currentMove === "O") {
+        id.style.color = "red";
+        currentMove = "X";
+    }
 }
 
-function addX(object) {
-    document.getElementById(object.id).innerHTML = "X";
+// Checks if user is using firefox (since it is not that supported
+function checkBrowser() {
+    let userAgentString = navigator.userAgent;
+    let chromeAgent = userAgentString.indexOf("Chrome") > -1;
+    let fireFoxAgent = userAgentString.indexOf("Firefox") > -1;
+    if (chromeAgent) {
+        console.log("User is using chrome!");
+    } else if (fireFoxAgent) {
+        window.alert("Warning: you may experience bugs using Firefox on this website! Consider using Google Chrome for the best experience :)");
+    }
 }
+
 
